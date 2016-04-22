c979a349f8d841f4596a74eb64d2079567c5efab
Chris Nolet <chris@relaunch.io>
2016-04-21 17:52:29 -0700

    Create basic layout

---
+++ Balance/HomeViewController.h
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.h
+//  Balance
+//
+//  Created by Chris Nolet on 4/21/16.
+//  Copyright © 2016 Relaunch. All rights reserved.
+//
+
+@interface HomeViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
+- (IBAction)addButtonPressed:(id)sender;
+
+@end
