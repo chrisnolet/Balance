d7b128cd5c2f94676cbfbd583e8277911504c695
Chris Nolet <chris@relaunch.io>
2016-04-27 16:27:50 -0700

    Create account selection controller

---
+++ Balance/Controllers/AddAccountTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddAccountTableViewController.h
+//  Balance
+//
+//  Created by Chris Nolet on 4/27/16.
+//  Copyright © 2016 Relaunch. All rights reserved.
+//
+
+@interface AddAccountTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *accounts;
+
+@end
