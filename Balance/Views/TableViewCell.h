b27af7ab545378718d81646f386dd69762ce1bf0
Chris Nolet <chris@relaunch.io>
2016-05-19 09:55:26 -0700

    Connect table view cell labels

---
+++ Balance/Views/TableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  TableViewCell.h
+//  Balance
+//
+//  Created by Chris Nolet on 5/19/16.
+//  Copyright © 2016 Relaunch. All rights reserved.
+//
+
+@interface TableViewCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *textLabel;
+@property (strong, nonatomic) IBOutlet UILabel *detailTextLabel;
+
+@end
---
+++ Balance/Views/TableViewCell.m
@@ -0,0 +1,16 @@
+//
+//  TableViewCell.m
+//  Balance
+//
+//  Created by Chris Nolet on 5/19/16.
+//  Copyright © 2016 Relaunch. All rights reserved.
+//
+
+#import "TableViewCell.h"
+
+@implementation TableViewCell
+
+@synthesize textLabel;
+@synthesize detailTextLabel;
+
+@end
